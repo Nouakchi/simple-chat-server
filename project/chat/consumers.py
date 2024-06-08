7476e0d8e74aa8be4b249a354af946c1b82de90d
Nouakchi <Othman.nouakchi007@gmail.com>
2024-06-08 16:50:03 +0100

    [Feat] Sending messages with username to define the sender and receiver

--- project/chat/consumers.py
+++ project/chat/consumers.py
@@ -27,15 +27,17 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
+        user = text_data_json["user"]
 
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name, {"type": "chat.message", "message": message}
+            self.room_group_name, {"type": "chat.message", "message": message, "user": user}
         )
 
     # Receive message from room group
     def chat_message(self, event):
         message = event["message"]
+        user = event["user"]
 
         # Send message to WebSocket
-        self.send(text_data=json.dumps({"message": message}))
+        self.send(text_data=json.dumps({"message": message, "user": user}))
